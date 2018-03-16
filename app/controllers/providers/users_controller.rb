db8090f645a21210cad20665b0fd932c7a50babb
Nicholas Utikal <nicholasutikal@gmail.com>
2018-03-16 23:27:10 +0100

    working on namespace issue

--- app/controllers/providers/users_controller.rb
+++ app/controllers/providers/users_controller.rb
@@ -1,2 +1,16 @@
 class Providers::UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+  def create
+    @user = User.create(user_params)
+    @user.save
+
+  end
+
+  private
+  def user_params
+    params.require(:user).permit([:first_name, :last_name, :email])
+
+  end
 end
