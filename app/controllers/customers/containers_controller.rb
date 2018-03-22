87a002a2fc9a02a17f4a162bc69c973d4dbfe129
Nicholas Utikal <nicholasutikal@gmail.com>
2018-03-22 12:27:10 +0100

    customer Pundit working

--- app/controllers/customers/containers_controller.rb
+++ app/controllers/customers/containers_controller.rb
@@ -1,7 +1,7 @@
 class Customers::ContainersController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
   def index
-    @containers = Container.all
+    @containers = policy_scope(Container).order(created_at: :desc)
     @containers = Container.where.not(latitude: nil, longitude: nil)
 
     @markers = @containers.map do |container|
@@ -16,13 +16,14 @@ class Customers::ContainersController < ApplicationController
     @container = Container.find(params[:id])
   #@visit = Visit.where(user: current_user, container: @container).first
   @container = Container.find(params[:id])
-    @markers =
-    [{
-      lat: @container.latitude,
-      lng: @container.longitude,
-      icon: '/toilet-marker.png',
-      draggable: false
-      }]
-  @visit = Visit.new
-end
+  @markers =
+  [{
+    lat: @container.latitude,
+    lng: @container.longitude,
+    icon: '/toilet-marker.png',
+    draggable: false
+    }]
+    @visit = Visit.new
+    authorize @container
+  end
 end
