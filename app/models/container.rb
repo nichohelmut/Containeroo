5eb630be058b8747d0ff29b5c8347a38ef932131
Nicholas Utikal <nicholasutikal@gmail.com>
2018-03-23 15:23:01 +0100

    locaition logo changes acording to user input (restaurant or supermarket)

--- app/models/container.rb
+++ app/models/container.rb
@@ -4,7 +4,7 @@ class Container < ApplicationRecord
   has_many :visits
   validates :product_category, inclusion: { in: %w(vegetables meats fruits bread dairy liquid beer cans ingredients starchy)}
   #validates :address, presence: true
-  validates :supermarket, inclusion: { in: %w(Private Restaurant Edeka Aldi Real Metro Lidl Spar Netto Plus TanteEmma)}
+  validates :supermarket, inclusion: { in: %w(Restaurant Edeka Aldi Real Metro Lidl Spar Netto Plus TanteEmma)}
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 end
