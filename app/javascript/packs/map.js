0b2409e6bd2e9d42882b73af639fbd7c59ee2850
Nicholas Utikal <nicholasutikal@gmail.com>
2018-04-11 19:13:20 +0200

    working on InfoWindow JS

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -9,45 +9,61 @@ var icon = {
     scaledSize: new google.maps.Size(40, 40), // scaled size
     origin: new google.maps.Point(0,0), // origin
     anchor: new google.maps.Point(0, 0) // anchor
-};
+  };
 
-var user_icon = {
+  var user_icon = {
     // url: "https://image.flaticon.com/icons/svg/603/603401.svg", // url
     url: "https://image.flaticon.com/icons/svg/10/10522.svg",
     scaledSize: new google.maps.Size(40, 40), // scaled size
     origin: new google.maps.Point(0,0), // origin
     anchor: new google.maps.Point(0, 0) // anchor
-};
+  };
 
-const mapElement = document.getElementById('map');
-console.debug(mapElement);
-const markers = JSON.parse(mapElement.dataset.markers);
-const user_markers = JSON.parse(mapElement.dataset.user_markers)
+  const mapElement = document.getElementById('map');
+  console.debug(mapElement);
+  const markers = JSON.parse(mapElement.dataset.markers);
+  const user_markers = JSON.parse(mapElement.dataset.user_markers)
+
+
+
 if (mapElement) { // don't try to build a map if there's no div#map to inject in
   const map = new GMaps({ el: '#map', lat: 0, lng: 0 });
-  console.log(markers);
-  markers.forEach(function(marker) {
-    map.addMarker({
-      lng: marker.lng,
-      lat: marker.lat,
-      icon: icon,
-    })
-  });
+console.log(markers);
+markers.forEach(function(marker) {
+  map.addMarker({
+    lng: marker.lng,
+    lat: marker.lat,
+    icon: icon,
+  })
+});
 
 
+// var infowindow = new google.maps.InfoWindow({
+//     content:"Hello World!"
+//   });
+
+// google.maps.event.addListener(markers, 'click', function() {
+//   infowindow.open(map,markers);
+// });
+
+// google.maps.event.addListener(markers, 'click', (function(infowindow) {
+//         return function() {
+//             infowindow.open(map,markers);
+//             content: "Hello World!"
+//         }
+//     })(infowindow)
+// );
 
 
-  if (markers.length === 0) {
-    map.setZoom(2);
-  } else if (markers.length === 1) {
-    map.setCenter(markers[0].lat, markers[0].lng);
+if (markers.length === 0) {
+  map.setZoom(2);
+} else if (markers.length === 1) {
+  map.setCenter(markers[0].lat, markers[0].lng);
     //console.log(map.setZoom(14));
     map.setZoom(14);
   } else {
     map.fitLatLngBounds(markers);
   }
-
-
 
 
   console.log(user_markers);
@@ -71,5 +87,8 @@ if (mapElement) { // don't try to build a map if there's no div#map to inject in
   }
 
 }
+
+
+
 
 
